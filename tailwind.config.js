3df2c1406446b3be26f3a000dd4e23c4c4e989fb
Rishi Tiwari <umesh896073@gmail.com>
2025-08-23 23:01:47 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // tell Tailwind where to look for classes,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
